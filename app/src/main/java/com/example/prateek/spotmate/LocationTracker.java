48de49790b304babb8235460e1f7f6dce3517fee
nilesh4145 <201452004@iiitvadodara.ac.in>
2016-06-07 23:59:53 +0530

    Defines Register layout

--- app/src/main/java/com/example/prateek/spotmate/LocationTracker.java
+++ app/src/main/java/com/example/prateek/spotmate/LocationTracker.java
@@ -10,9 +10,6 @@ import android.location.LocationManager;
  * Created by nilesh on 7/6/16.
  */
 public class LocationTracker extends Service implements LocationListener {
-
-    private final Context mContext;
-
     /**
      * A function to check network availability;
      * @return
@@ -22,22 +19,7 @@ public class LocationTracker extends Service implements LocationListener {
                 locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
     }
 
-    boolean canGetLocation=false;
-
-    Location location;
-    double latitude;
-    double longitude;
-
-    private static final long distanceIntercept=10; // Minimum distance intercept to update the table with new location.(10 meters)
-
-    private static final long timeIntercept=30*1000; // Time interval at which the location is to be updated. i.e. 30sec.
-
-    private LocationManager locationManager;
-
-    public LocationTracker(Context context){
-        this.mContext=context;
-        getLocation();
-    }
+        }
 
 
 }
