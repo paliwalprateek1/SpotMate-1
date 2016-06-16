dd6f67ce86e17cc9c358a51a9f073780d5a885e6
nilesh4145 <201452004@iiitvadodara.ac.in>
2016-06-16 20:55:26 +0530

    initialized locationManager and listener

--- app/src/main/java/com/example/prateek/spotmate/home.java
+++ app/src/main/java/com/example/prateek/spotmate/home.java
@@ -1,15 +1,27 @@
 package com.example.prateek.spotmate;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.media.audiofx.BassBoost;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.location.LocationManager;
+import android.location.LocationListener;
+import android.location.LocationProvider;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.math.BigInteger;
@@ -21,20 +33,57 @@ import java.security.NoSuchAlgorithmException;
 public class home extends AppCompatActivity {
     int i = 1;
     String username;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Toast.makeText(this, "jezzz", Toast.LENGTH_SHORT).show();
         username = getIntent().getStringExtra("username");
+*/      button = (Button) findViewById(R.id.button1);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Toast.makeText(home.this, location.getLatitude()+  " " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+            }
 
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
 
+            }
 
-//        double lat=28.6129;
-//        double lng=77.2295;
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+
+            }
+        };
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET
+                }, 10 );
+                return;
+            }
+        }else{
+            configureButton();
+        }
+
 //
 //        String geoUri = "http://maps.google.com/maps?q=loc:" + lat + "," + lng ;
 //
@@ -42,6 +91,28 @@ public class home extends AppCompatActivity {
 //        startActivity(i);
 
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 10:
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    configureButton();
+                return;
+
+        }
+    }
+
+    private void configureButton() {
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+
+            }
+        });
     }
 
     public String MD5_hash(String input) {
@@ -78,10 +149,10 @@ public class home extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.refresh) {
-            Intent intent = new Intent();
-            intent.setClass(this, GetLocationStatus.class);
-            intent.putExtra("username", username);
-            startActivity(intent);
+//            Intent intent = new Intent();
+//            intent.setClass(this, GetLocationStatus.class);
+//            intent.putExtra("username", username);
+//            startActivity(intent);
             return true;
         }
 
